42311a8e60a0bf9f9c0cf55d5a449c91bcb84fa9
Sumeet Singh <sumeet.singhji@outlook.com>
2024-03-22 15:45:31 +1100

    v0.1

--- website/scripts/webserver.mjs
+++ website/scripts/webserver.mjs
@@ -3,6 +3,17 @@ import fs from 'fs';
 import path from 'path';
 import url from 'url';
 
+/* 
+folder structure is as follows
+
+note relative to this file, the website files are one level up hence websitePath = '..'
+
+C:\Users\Sumeet\Documents\AgniSamooh\website\index.html
+C:\Users\Sumeet\Documents\AgniSamooh\website\css\agnisamooh.css
+C:\Users\Sumeet\Documents\AgniSamooh\website\scripts\agnisamooh.js
+C:\Users\Sumeet\Documents\AgniSamooh\website\scripts\webserver.mjs
+*/
+
 const cwd = process.cwd(); // CWD: C:\Users\Sumeet\Documents\AgniSamooh\website\scripts
 const websitePath = path.resolve(cwd, '..'); // Absolute: C:\Users\Sumeet\Documents\AgniSamooh\website
 console.log("websitePath is: " + websitePath);
@@ -20,7 +31,25 @@ http.createServer(function (req, res) {
     console.log('filename is: ' + filename);
 
     var filepath = path.join(websitePath, filename); // e.g., C:\Users\Sumeet\Documents\AgniSamooh\website\index.html
+
     console.log('filepath to read is: ' + filepath);
+
+    // Determine content type based on file extension to serve linked sheets in .html back to client e.g. .css, .js
+    var contentType;
+    switch (path.extname(filepath)) {
+        case '.html':
+            contentType = 'text/html';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        default:
+            contentType = 'text/plain';
+    }
+    console.log('Content-Type is: ' + contentType);
 
     fs.readFile(filepath, function(err, data) { // e.g., C:\Users\Sumeet\Documents\AgniSamooh\website\index.html
         if (err) {
@@ -28,16 +57,17 @@ http.createServer(function (req, res) {
             fs.readFile(path.join(websitePath, 'error.html'), function(err, errorPageContents) {
                 if (err) {
                     res.writeHead(500, {'Content-Type': 'text/html'});
-                    res.end('Internal Server Error');
+                    res.write('HTTP error code: 500: Internal Server Error');
+                    res.end();
                 } else {
-                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    res.writeHead(404, {'Content-Type': contentType});
                     res.write(errorPageContents);
                     res.end();
                 }
             });
         } else {
             // If the file is successfully read, serve its contents
-            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.writeHead(200, {'Content-Type': contentType});
             res.write(data);
             res.end();
         }
